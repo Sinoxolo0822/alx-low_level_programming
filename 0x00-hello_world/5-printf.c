1a16594d9cf1303523b5c5fc8ae3d798a36591d8
Sinoxolo0822 <snoxolo.komityi@gmail.com>
2023-08-13 11:29:02 -0700

    this is printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - A profram that prints a line using the printf fuction
  * Return: 0 (Sussess)
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n*)
+	printf ("with proper grammar, but the outcome is a piece of art,\n*);
 		return (0);
 }
