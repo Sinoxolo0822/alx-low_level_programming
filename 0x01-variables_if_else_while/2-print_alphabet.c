040840dd8bde649dab1ffe4abd4e97893afe4ec7
Sinoxolo0822 <snoxolo.komityi@gmail.com>
2023-08-15 07:51:03 -0700

    Since Yesterday

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = o; i < 26; i++)
+	{
+		patchar(alp[i]);
+	}
+	patchar('\n');
+	return (0);
+}
