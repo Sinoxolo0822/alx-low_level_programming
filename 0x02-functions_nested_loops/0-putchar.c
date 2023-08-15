be0a1f2386f5745b5aabd213fdd10c2f7cc86644
Sinoxolo0822 <snoxolo.komityi@gmail.com>
2023-08-15 12:34:35 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	putchar(");
-	-putchar("p");
-	-putchar("u");
-	-putchar("t");
-	-putchar("c");
-	-putchar("h");
-	-putchar("a");
-	-putchar("r");
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
 	-putchar('\n');
 	return (0);
 }
